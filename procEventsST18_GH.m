865be8db35eb336c6e1be662bfc42a56da4a5c54
Tamar Regev <tamaregev@gmail.com>
2018-09-27 15:55:58 +0300

    tidy-up
    removing unnecessary comments

--- procEventsST18_GH.m
+++ procEventsST18_GH.m
@@ -1,10 +1,6 @@
 function [ ERPtrialsTot ] = procEventsST18_GH(blocks, mode, SaveFolder, gammaflag, ampflag)
 % written by Tamar Regev, lab of prof. Leon Deouell, HUJI
 
-% 18/11/2014 - adapted from L:\Experiments\durationGamma\ECOG\Original Exp\Results\ST18\VisualStream\fastPreProc\ST18_procEvents.m  
-% for use in - MasterScript_VOLST18
-% 14/12/2014 - Tamar: added gammaflag just for sending to
-% procEventsST18_block, where the actual calculation is done.
 %%
     win=-299:300;
     ntrials_block = cell(length(blocks));
@@ -34,7 +30,6 @@ function [ ERPtrialsTot ] = procEventsST18_GH(blocks, mode, SaveFolder, gammafla
         clear(['ERPtrials' block])
     end
     %% baseline subtract from ERPtrialsTot
-    %this is the old matrix containing no targets and metadata
     baselinewin=1:(1-win(1));
     baseline = ERPtrialsTot(:,baselinewin,:);
     memutsa=mean(baseline,2);
